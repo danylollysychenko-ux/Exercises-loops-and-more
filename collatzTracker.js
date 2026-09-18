b2fb22226a8bf0bf9d752955144543dba560b085
danylollysychenko-ux <danylollysychenko@gmail.com>
2026-09-18 12:42:02 -0400

    Work on this

---
+++ collatzTracker.js
@@ -0,0 +1,21 @@
+let number ="";
+let numEntered;
+
+while (number < 0){
+    number = prompt("Enter a positive number");
+    if (number > 0){
+        break;
+    }
+}
+
+// while (number < 0) {
+//   number = prompt("Enter a positive number");
+//   if (number < 0) {
+//     let numEntered = number;
+//   }
+// }
+
+// console.log(numEntered);
+// for (let i = 0; i < 50; i++) {
+//   console.log(i);
+// }
